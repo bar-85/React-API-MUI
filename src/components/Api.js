3d5761ccdf56a205929458dcba3a4a9b0a8cacdb
bar-85 <bajwolskabarbara@gmail.com>
2022-11-30 00:43:22 +0100

    add styles to the table

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material'
 
-
 const Api = () => {
 	const usersUrl = 'https://jsonplaceholder.typicode.com/users'
 
@@ -16,34 +15,32 @@ const Api = () => {
 	}, [])
 
 	return (
-		<TableContainer component={Paper} sx={{ maxHeight: '500px', width: 900, margin: 'auto'}} >
-			<Table aria-label='simple table' stickyHeader>
-				<TableHead>
-					<TableRow>
-						<TableCell>Id</TableCell>
-						<TableCell>Name</TableCell>
-						<TableCell>Email</TableCell>
-						<TableCell>Address</TableCell>
-						<TableCell>Phone</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{users.map(user => {
-						return (
-							<TableRow key={user.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 }}}
-                            >
-								<TableCell>{user.id}</TableCell>
-								<TableCell>{user.name}</TableCell>
-								<TableCell>{user.email}</TableCell>
-								<TableCell>{user.address.city}</TableCell>
-								<TableCell>{user.phone}</TableCell>
-							</TableRow>
-						)
-					})}
-				</TableBody>
-			</Table>
-		</TableContainer>
+			<TableContainer component={Paper} sx={{ maxHeight: '500px', width: '90%', margin: 'auto' }}>
+				<Table aria-label='simple table' stickyHeader>
+					<TableHead>
+						<TableRow>
+							<TableCell>Id</TableCell>
+							<TableCell>Name</TableCell>
+							<TableCell>Email</TableCell>
+							<TableCell>Address</TableCell>
+							<TableCell>Phone</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{users.map(user => {
+							return (
+								<TableRow key={user.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+									<TableCell>{user.id}</TableCell>
+									<TableCell>{user.name}</TableCell>
+									<TableCell>{user.email}</TableCell>
+									<TableCell>{user.address.city}</TableCell>
+									<TableCell>{user.phone}</TableCell>
+								</TableRow>
+							)
+						})}
+					</TableBody>
+				</Table>
+			</TableContainer>
 	)
 }
 
