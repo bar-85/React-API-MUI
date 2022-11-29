4b7b528d355f14fe77d5c5cd03ab750d87072728
bar-85 <bajwolskabarbara@gmail.com>
2022-11-30 00:47:20 +0100

    Correct the spacing and quotation marks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Api from "./components/Api";
-import Header from "./components/Header";
+import Api from './components/Api'
+import Header from './components/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Api />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Api />
+		</div>
+	)
 }
 
-export default App;
+export default App
